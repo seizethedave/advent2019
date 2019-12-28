9d9c168a33958aa238170f226d9d4444c34c204a
David Grant <david.grant@mixpanel.com>
2019-12-28 12:38:49 -0800

    don't need to compute all run lengths in digits.

--- advent04/advent04b/main.go
+++ advent04/advent04b/main.go
@@ -7,35 +7,24 @@ import (
 	"github.com/seizethedave/advent2019/advent04"
 )
 
-// compute a list of "run lengths".
-// If the input is [1 1 1 1 2 2], there is a run of 4 1's and 2 2's => [4 2].
-// [5 6 7 7 7 7] => [1 1 4] etc.
-func runLengths(digits []byte) []int {
-	var lengths []int
+func hasRunOfLength2(digits []byte) bool {
 	length := 1
 	prev := digits[0]
+
+	// Any time the running digit changes, see if the run length was 2.
 
 	for i := 1; i < len(digits); i++ {
 		if digits[i] == prev {
 			length++
 		} else {
-			lengths = append(lengths, length)
+			if length == 2 {
+				return true
+			}
 			length = 1
 			prev = digits[i]
 		}
 	}
-
-	lengths = append(lengths, length)
-	return lengths
-}
-
-func hasRunOfLength2(digits []byte) bool {
-	for _, length := range runLengths(digits) {
-		if length == 2 {
-			return true
-		}
-	}
-	return false
+	return length == 2
 }
 
 func main() {
