45309f4f11683cc9c1676b80d03c68a985c25216
David Grant <david.grant@mixpanel.com>
2019-12-27 20:51:04 -0800

    go fmt

--- advent04/advent04a/main.go
+++ advent04/advent04a/main.go
@@ -1,10 +1,10 @@
-package advent04
+package main
 
 import (
 	"bytes"
 	"fmt"
 
-  password "github.com/seizethedave/advent2019/advent04"
+	"github.com/seizethedave/advent2019/advent04"
 )
 
 func hasRepeat(digits []byte) bool {
@@ -18,12 +18,12 @@ func hasRepeat(digits []byte) bool {
 
 func main() {
 	// given was 124075, 124444 was next proper value.
-  digits := []byte{1, 2, 4, 4, 4, 4}
+	digits := []byte{1, 2, 4, 4, 4, 4}
 	end := []byte{5, 8, 0, 7, 6, 9}
 	num := 0
 
-	for ; bytes.Compare(digits, end) <= 0; password.Incr(digits) {
-    if hasRepeat(digits) {
+	for ; bytes.Compare(digits, end) <= 0; advent04.Incr(digits) {
+		if hasRepeat(digits) {
 			num++
 		}
 	}
